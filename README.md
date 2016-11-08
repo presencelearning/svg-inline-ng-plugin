f3efc7996da691a12b55059e25d768c756c166c7
Luke Madera <luke.madera@gmail.com>
2016-11-08 15:20:15 -0800

    first version

---
+++ README.md
@@ -0,0 +1,57 @@
+# SVG Inline Angular Plugin
+
+Takes a directory of .svg files and generates an angular service file with the svg code for inline use.
+
+
+## Why?
+
+`<object>` svg's can not be styled with CSS easily.
+To get dynamic sizing and CSS (color fill) styling, inline `<svg>` is the best option.
+But for ease of use we want just use files.
+So this takes the files and creates an Angular service with the inlined svg code for use.
+
+https://svgontheweb.com/#implementation
+https://css-tricks.com/using-svg/
+
+
+## Usage
+
+1. Add to webpack config:
+
+```
+var SvgInlineNgPlugin = require('svg-inline-ng-plugin');
+...
+new SvgInlineNgPlugin({
+    svgDir: helpers.root('public', 'images', 'svg'),
+    writeDir: helpers.root('src', 'build')
+})
+```
+
+2. Import our generated file and use the svg code.
+
+```
+import { SvgInlineNgPluginService } from '../../../../build/svg-inline-ng-plugin.service';
+...
+providers: [SvgInlineNgPluginService]
+...
+constructor(private sanitizer: DomSanitizer,
+ private svgInlineNgPluginService: SvgInlineNgPluginService ) {}
+...
+this.svgCode = this.sanitizer.bypassSecurityTrustHtml(this.svgInlineNgPluginService.svgs[this.svg].html);
+```
+
+And then in the html:
+```
+<div [innerHtml]="svgCode" class="pl-icon-svg"></div>
+```
+
+3. Ignore the generated file to prevent webpack build loops.
+
+```
+var WatchIgnorePlugin = webpack.WatchIgnorePlugin;
+...
+new WatchIgnorePlugin([
+    helpers.root('src', 'build')
+]),
+```
+
---
+++ index.js
@@ -0,0 +1,135 @@
+var path = require('path');
+var fs = require('fs');
+var async = require('async');
+var tabChar = '    ';
+
+function SvgInlineNgPlugin(options) {
+    if (typeof options !== "object") {
+        options = {};
+    }
+    this.options = options;
+}
+
+function headerHtml() {
+    var html = `/*\n` +
+        `## Usage:\n` +
+        `1. Optimize SVG with SVGO\n` +
+        `${tabChar}1. Typically, make sure the viewbox is a square (height same as width, with icon centered).\n` +
+        `${tabChar}2. https://jakearchibald.github.io/svgomg/\n` +
+        `${tabChar}3. REMOVE any height & width attributes on the svg. Optionally add 'width="100%"' instead.\n` +
+        `${tabChar}4. Use / add explicit close tags, e.g. '<path></path>' instead of '<path />'.\n` +
+        `${tabChar}5. Remove any 'fill' or other styling you want to use css for.\n` +
+        `2. Save your .svg files in a directory for this plugin to inline the svg code:\n` +
+        `${tabChar}1. find and replace regex '\\n' with '' to remove new lines\n` +
+        `3. the SVG will be responsive and resize based on the element dimensions; use CSS as normal to set width / height\n` +
+        `*/\n\n` +
+        `export class SvgInlineNgPluginService {\n` +
+        `${tabChar}svgs: any = {\n`;
+    return html;
+}
+
+function footerHtml() {
+    var html = `${tabChar}};\n\n` +
+        `}`;
+    return html;
+}
+
+function readFiles(dirname, callback) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      callback(err);
+    }
+    var svgName;
+    var svgContent;
+    var html = ``;
+    async.each(filenames, function(filename, callback) {
+        fs.readFile(dirname + '/' + filename, 'utf-8', function(err, content) {
+            if (err) {
+                console.log('err: ', err);
+                callback(err);
+            } else {
+                if (filename.indexOf('.svg') > -1) {
+                    // TODO - check for valid characters in file name? E.g. no spaces.
+                    svgName = filename.slice(0, filename.lastIndexOf('.'));
+                    svgContent = content.replace(/\n/g, '');
+                    html += `${tabChar}${tabChar}'${svgName}': {\n` + 
+                        `${tabChar}${tabChar}${tabChar}html: '${svgContent}'\n` +
+                        `${tabChar}${tabChar}},\n`;
+                }
+                callback(false);
+            }
+          });
+    }, function(err, results) {
+        callback(err, html);
+    });
+  });
+}
+
+// https://github.com/pingyuanChen/webpack-uglify-js-plugin/blob/master/utils/file.js#L10
+// Like mkdir -p. Create a directory and any intermediary directories.
+function mkdir (dirpath, mode) {
+  // Set directory mode in a strict-mode-friendly way.
+  if (mode == null) {
+    mode = parseInt('0777', 8) & (~process.umask());
+  }
+  var pathSeparatorRe = /[\/\\]/g;
+  dirpath.split(pathSeparatorRe).reduce(function(parts, part) {
+    parts += part + '/';
+    var subpath = path.resolve(parts);
+    if (!fs.existsSync(subpath)) {
+      try {
+        fs.mkdirSync(subpath, mode);
+      } catch(e) {
+        throw console.error('Unable to create directory "' + subpath + '" (Error code: ' + e.code + ').', e);
+      }
+    }
+    return parts;
+  }, '');
+}
+
+function writeFile(path, contents) {
+    // Create path, if necessary.
+    var dirPath = path.slice(0, path.lastIndexOf('/'));
+    console.log(dirPath);
+    mkdir(dirPath);
+    fs.writeFileSync(path, contents);
+}
+
+SvgInlineNgPlugin.prototype.apply = function(compiler) {
+    var options = this.options;
+    if (options.svgDir && options.writeDir) {
+        // compiler.plugin('emit', function(compilation, callback) {
+        // compiler.plugin('compile', function(params) {
+        compiler.plugin('compilation', function(compilation) {
+            console.log('YES');
+            var html = headerHtml();
+
+            readFiles(options.svgDir, function(err, svgHtml) {
+                if (err) {
+                    console.log('err: ', err);
+                    callback();
+                } else {
+                    var html = `${headerHtml()}${svgHtml}${footerHtml()}`;
+                    // console.log(html);
+                    var filepath = `${options.writeDir}/svg-inline-ng-plugin.service.ts`;
+                    writeFile(filepath, html);
+                    console.log('file written');
+                    // // Insert this list into the Webpack build as a new file asset:
+                    compilation.assets['svg-inline-ng-plugin.service.ts'] = {
+                      source: function() {
+                        return html;
+                      },
+                      size: function() {
+                        return html.length;
+                      }
+                    };
+                    // callback();
+                }
+            });
+        });
+    } else {
+        console.error(`svg-inline-ng-plugin: options.svgDir ${options.svgDir} and options.writeDir ${options.writeDir} are required.`);
+    }
+};
+
+module.exports = SvgInlineNgPlugin;
